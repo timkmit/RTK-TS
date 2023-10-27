69c327f104344e39701892d5ecf55aca27d84651
Timur <timengalytchev@mail.ru>
2023-10-27 22:03:42 +0300

    made increment on page, learned how it worked

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 
 import './App.css'
+import { useAppDispatch, useAppSelector } from './store/hooks/redux'
+import { userSlice } from './store/reducers/UserSlice'
+
 
 function App() {
 
+  const {count} = useAppSelector(state => state.userReducer)
+  const {increment} = userSlice.actions;
+  const dispatch = useAppDispatch();
+
+  console.log(increment(5))
+
   return (
     <>
-      
+      <div>
+        <h1>{count}</h1>
+        <button onClick={() => dispatch(increment(1))}>INC</button>
+      </div>
     </>
   )
 }
